b75603040932733868b59599b7b94b87fb47c20e
Adriano <adriano2147@gmail.com>
2021-09-16 19:48:05 -0300

    Terminei o joguinho

--- main.cpp
+++ main.cpp
@@ -1,88 +1,90 @@
 #include <iostream>
-#include <string>
 #include <random>
 #include <chrono>
 #include <array>
 #include <algorithm>
 #include <queue>
-#include <iomanip>
 #include "bit_helper.h"
 #include "main.h"
 
-enum CELL_FLAGS {
-    NEAR_BOMB = 0x0F,
-    BOMB = 0x10,
-    FLAG = 0x20,
-    HIDDEN = 0x40,
-};
-
-Game::Game(int w, int h, const char* title)
-    : grid {}
-    , window(sf::VideoMode(w, h), title, sf::Style::Close)
-    , game_over(false)
-    , ganhou(false)
+void Game::get_texture(sf::Vertex* v, int8_t cell) const
 {
-    generate_grid(grid);
-}
-
-void make_quad_pos(sf::Vertex* v, float ini, float end)
-{
-    v[0].position = { ini, ini };
-    v[1].position = { ini, end };
-    v[2].position = { end, end };
-    v[3].position = { end, ini };
-}
-
-void Game::get_texture(sf::Vertex* v, int8_t cell)
-{
-    const static sf::Color COLOR_HIDDEN = { 127, 127, 127, 255 };
+    const static sf::Color COLOR_CELL = sf::Color::White;
     const static sf::Color COLOR_ZERO = { 77, 77, 77, 255 };
-    const static sf::Color COLOR_BLUE = { 30, 30, 120, 255 };
-    const sf::Color COLOR_BOMB = ganhou ? sf::Color(20, 255, 20, 255) : sf::Color(255, 20, 20, 255);
+    const static sf::Color COLOR_GREEN = sf::Color::Green;
+    const static sf::Color COLOR_RED = sf::Color::Red;
 
     if (has(cell, FLAG)) {
-        v[0].color = COLOR_HIDDEN;
-        v[1].color = COLOR_HIDDEN;
-        v[2].color = COLOR_HIDDEN;
-        v[3].color = COLOR_HIDDEN;
-        v[0].texCoords = { 0.f, 0.f };
-        v[1].texCoords = { 0.f, CELL_SIZEF };
-        v[2].texCoords = { CELL_SIZEF, CELL_SIZEF };
-        v[3].texCoords = { CELL_SIZEF, 0.f };
+        float textureoffset = CELL_SIZEF_BORDER * 1.f;
+
+        v[0].color = COLOR_CELL;
+        v[1].color = COLOR_CELL;
+        v[2].color = COLOR_CELL;
+        v[3].color = COLOR_CELL;
+
+        v[0].texCoords = { textureoffset, 0.f };
+        v[1].texCoords = { textureoffset, CELL_SIZEF_BORDER };
+        v[2].texCoords = { textureoffset + CELL_SIZEF_BORDER, CELL_SIZEF_BORDER };
+        v[3].texCoords = { textureoffset + CELL_SIZEF_BORDER, 0.f };
     } else if (has(cell, HIDDEN)) {
-        v[0].color = COLOR_HIDDEN;
-        v[1].color = COLOR_HIDDEN;
-        v[2].color = COLOR_HIDDEN;
-        v[3].color = COLOR_HIDDEN;
-    } else if (has(cell, BOMB)) {
-        v[0].color = COLOR_BOMB;
-        v[1].color = COLOR_BOMB;
-        v[2].color = COLOR_BOMB;
-        v[3].color = COLOR_BOMB;
+        v[0].color = COLOR_CELL;
+        v[1].color = COLOR_CELL;
+        v[2].color = COLOR_CELL;
+        v[3].color = COLOR_CELL;
+
+        v[0].texCoords = { 0.f, 0.f };
+        v[1].texCoords = { 0.f, CELL_SIZEF_BORDER };
+        v[2].texCoords = { CELL_SIZEF_BORDER, CELL_SIZEF_BORDER };
+        v[3].texCoords = { CELL_SIZEF_BORDER, 0.f };
+    } else if (has(cell, BOMB) && !ganhou) {
+        v[0].color = COLOR_RED;
+        v[1].color = COLOR_RED;
+        v[2].color = COLOR_RED;
+        v[3].color = COLOR_RED;
+
+        v[0].texCoords = { 0.f, 0.f };
+        v[1].texCoords = { 0.f, CELL_SIZEF_BORDER };
+        v[2].texCoords = { CELL_SIZEF_BORDER, CELL_SIZEF_BORDER };
+        v[3].texCoords = { CELL_SIZEF_BORDER, 0.f };
+    } else if (has(cell, BOMB) && ganhou) {
+        v[0].color = COLOR_GREEN;
+        v[1].color = COLOR_GREEN;
+        v[2].color = COLOR_GREEN;
+        v[3].color = COLOR_GREEN;
+
+        v[0].texCoords = { 0.f, 0.f };
+        v[1].texCoords = { 0.f, CELL_SIZEF_BORDER };
+        v[2].texCoords = { CELL_SIZEF_BORDER, CELL_SIZEF_BORDER };
+        v[3].texCoords = { CELL_SIZEF_BORDER, 0.f };
     } else if (!has(cell, NEAR_BOMB)) {
         v[0].color = COLOR_ZERO;
         v[1].color = COLOR_ZERO;
         v[2].color = COLOR_ZERO;
         v[3].color = COLOR_ZERO;
-    } else {
-        v[0].color = COLOR_BLUE;
-        v[1].color = COLOR_BLUE;
-        v[2].color = COLOR_BLUE;
-        v[3].color = COLOR_BLUE;
 
-        float textureoffset = static_cast<float>(CELL_SIZEF_BORDER * has(cell, NEAR_BOMB));
+        v[0].texCoords = { 0.f, 0.f };
+        v[1].texCoords = { 0.f, CELL_SIZEF_BORDER };
+        v[2].texCoords = { CELL_SIZEF_BORDER, CELL_SIZEF_BORDER };
+        v[3].texCoords = { CELL_SIZEF_BORDER, 0.f };
+    } else {
+        float textureoffset = CELL_SIZEF_BORDER * (has(cell, NEAR_BOMB) + 1);
+
+        v[0].color = COLOR_CELL;
+        v[1].color = COLOR_CELL;
+        v[2].color = COLOR_CELL;
+        v[3].color = COLOR_CELL;
 
         v[0].texCoords = { textureoffset, 0.f };
         v[1].texCoords = { textureoffset, CELL_SIZEF_BORDER };
-        v[2].texCoords = { textureoffset + 19.f, CELL_SIZEF_BORDER };
-        v[3].texCoords = { textureoffset + 19.f, 0.f };
+        v[2].texCoords = { textureoffset + CELL_SIZEF_BORDER, CELL_SIZEF_BORDER };
+        v[3].texCoords = { textureoffset + CELL_SIZEF_BORDER, 0.f };
     }
 }
 
-bool verifica_ganhou(const Grid& grid)
+bool Game::verifica_ganhou() const
 {
-    for (int i = 0; i < GRID_ROW; i++) {
-        for (int j = 0; j < GRID_COL; j++) {
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < col; j++) {
             int8_t val = grid[i][j];
 
             if (has(val, HIDDEN) && has(val, NEAR_BOMB)) {
@@ -98,35 +100,37 @@ bool verifica_ganhou(const Grid& grid)
     return true;
 }
 
-std::array<sf::Vertex, 4 * GRID_ROW * GRID_COL> vertices;
-
 void Game::draw_grid()
 {
-    vertices = {};
-
-    for (int i = 0; i < GRID_ROW; i++) {
-        float posi = CELL_SIZEF_BORDER * i;
-
-        for (int j = 0; j < GRID_COL; j++) {
-            float posj = CELL_SIZEF_BORDER * j;
-
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < col; j++) {
             int8_t val = grid[i][j];
-
-            sf::Vertex* vertex = &vertices[(j + i * GRID_ROW) * 4];
-
-            vertex[0].position = { 0.f + posj, 0.f + posi };
-            vertex[1].position = { 0.f + posj, 19.f + posi };
-            vertex[2].position = { 19.f + posj, 19.f + posi };
-            vertex[3].position = { 19.f + posj, 0.f + posi };
-
+            sf::Vertex* vertex = &vertices[(i + j * row) * 4];
             get_texture(vertex, val);
         }
     }
 
     sf::RenderStates state;
     state.texture = &texture;
-
     window.draw(&vertices[0], vertices.size(), sf::Quads, state);
+}
+
+void Game::set_positions()
+{
+    for (int i = 0; i < row; i++) {
+        float posi = CELL_SIZEF_BORDER * i;
+
+        for (int j = 0; j < col; j++) {
+            float posj = CELL_SIZEF_BORDER * j;
+
+            sf::Vertex* vertex = &vertices[(i + j * row) * 4];
+
+            vertex[0].position = { 0.f + posj, 0.f + posi };
+            vertex[1].position = { 0.f + posj, 19.f + posi };
+            vertex[2].position = { 19.f + posj, 19.f + posi };
+            vertex[3].position = { 19.f + posj, 0.f + posi };
+        }
+    }
 }
 
 void Game::render()
@@ -136,41 +140,41 @@ void Game::render()
     window.display();
 }
 
-void generate_grid(Grid& grid)
+void Game::generate_grid()
 {
     static std::random_device randomDevice;
     static std::default_random_engine engine(randomDevice());
-    std::array<std::array<int8_t, 2>, GRID_COL * GRID_ROW> indexes;
+    std::vector<std::array<int8_t, 2>> indexes(row * col);
 
-    std::fill_n(grid.begin()->begin(), GRID_COL * GRID_ROW, HIDDEN);
+    std::fill_n(&grid[0][0], row * col, static_cast<int8_t>(HIDDEN));
 
     size_t n = 0;
 
-    for (int8_t i = 0; i < GRID_ROW; i++) {
-        for (int8_t j = 0; j < GRID_COL; j++) {
+    for (int8_t i = 0; i < row; i++) {
+        for (int8_t j = 0; j < col; j++) {
             indexes[n++] = { i, j };
         }
     }
 
     std::shuffle(indexes.begin(), indexes.end(), engine);
 
-    for (size_t i = 0; i < NUM_BOMBS; i++) {
+    for (size_t i = 0; i < num_bombs; i++) {
         set(grid[indexes[i][0]][indexes[i][1]], BOMB);
     }
 
     constexpr int nextRows[] = { -1, -1, -1, 0, 0, 1, 1, 1 };
     constexpr int nextCols[] = { -1, 0, 1, -1, 1, -1, 0, 1 };
 
-    for (int i = 0; i < GRID_ROW; i++) {
-        for (int j = 0; j < GRID_COL; j++) {
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < col; j++) {
             if (has(grid[i][j], BOMB)) {
                 for (int k = 0; k < 8; k++) {
-                    int row = i + nextRows[k];
-                    int col = j + nextCols[k];
+                    int row_i = i + nextRows[k];
+                    int col_i = j + nextCols[k];
 
-                    if (row >= 0 && row < GRID_ROW && col >= 0 && col < GRID_COL) {
-                        if (!has(grid[row][col], BOMB)) {
-                            grid[row][col]++;
+                    if (row_i >= 0 && row_i < row && col_i >= 0 && col_i < col) {
+                        if (!has(grid[row_i][col_i], BOMB)) {
+                            grid[row_i][col_i]++;
                         }
                     }
                 }
@@ -179,19 +183,7 @@ void generate_grid(Grid& grid)
     }
 }
 
-void print_values(const Grid& grid)
-{
-    for (int i = 0; i < GRID_ROW; i++) {
-        for (int j = 0; j < GRID_COL; j++) {
-            int8_t val = grid[i][j];
-            std::cout << std::setw(2) << (int)val << ' ';
-        }
-
-        std::cout << '\n';
-    }
-}
-
-void open_cells(Grid& grid, int startRow, int startCol)
+void Game::open_cells(int startRow, int startCol)
 {
     constexpr int nextRows[] = { -1, -1, -1, 0, 0, 0, 1, 1, 1 };
     constexpr int nextCols[] = { -1, 0, 1, -1, 0, 1, -1, 0, 1 };
@@ -210,12 +202,11 @@ void open_cells(Grid& grid, int startRow, int startCol)
 
             if (!(val & BOMB) && !(val & 0x0F)) {
                 for (int i = 0; i < 9; i++) {
-                    int row = idx[0] + nextRows[i];
-                    int col = idx[1] + nextCols[i];
+                    int row_i = idx[0] + nextRows[i];
+                    int col_i = idx[1] + nextCols[i];
 
-                    if (row >= 0 && row < GRID_ROW
-                        && col >= 0 && col < GRID_COL) {
-                        q.push({ row, col });
+                    if (row_i >= 0 && row_i < row && col_i >= 0 && col_i < col) {
+                        q.push({ row_i, col_i });
                     }
                 }
             }
@@ -223,10 +214,10 @@ void open_cells(Grid& grid, int startRow, int startCol)
     }
 }
 
-void display_bombs(Grid& grid)
+void Game::display_bombs()
 {
-    for (int i = 0; i < GRID_ROW; i++) {
-        for (int j = 0; j < GRID_COL; j++) {
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < col; j++) {
             if (has(grid[i][j], BOMB)) {
                 clear(grid[i][j], FLAG);
                 clear(grid[i][j], HIDDEN);
@@ -235,9 +226,9 @@ void display_bombs(Grid& grid)
     }
 }
 
-void handle_mouse_release(const sf::Event& ev, Game& g)
+void Game::handle_mouse_release(const sf::Event& ev)
 {
-    if (g.game_over) {
+    if (game_over) {
         return;
     }
 
@@ -250,7 +241,7 @@ void handle_mouse_release(const sf::Event& ev, Game& g)
     const int remY = posY % CELL_SIZE_BORDER;
 
     if (remX < CELL_SIZE && remY < CELL_SIZE) {
-        int8_t& val = g.grid[cellY][cellX];
+        int8_t& val = grid[cellY][cellX];
 
         if (ev.mouseButton.button == sf::Mouse::Button::Right) {
             if (has(val, HIDDEN)) {
@@ -259,66 +250,136 @@ void handle_mouse_release(const sf::Event& ev, Game& g)
         } else if (ev.mouseButton.button == sf::Mouse::Button::Left) {
             if (!has(val, FLAG)) {
                 if (has(val, BOMB)) {
-                    g.game_over = true;
-                    display_bombs(g.grid);
+                    game_over = true;
+                    display_bombs();
                     return;
                 } else if (has(val, HIDDEN)) {
-                    open_cells(g.grid, cellY, cellX);
+                    open_cells(cellY, cellX);
                 }
             }
         }
     }
 
-    if (verifica_ganhou(g.grid)) {
-        g.ganhou = true;
-        g.game_over = true;
-        display_bombs(g.grid);
+    if (verifica_ganhou()) {
+        ganhou = true;
+        game_over = true;
+        display_bombs();
     }
 }
 
-void handle_keyboard(const sf::Event& ev, Game& g)
+void Game::handle_keyboard(const sf::Event& ev)
 {
     switch (ev.key.code) {
     case sf::Keyboard::Enter:
-        g.game_over = false;
-        g.ganhou = false;
-        g.grid = {};
-        generate_grid(g.grid);
+        game_over = false;
+        ganhou = false;
+        generate_grid();
         break;
+
     case sf::Keyboard::Escape:
-        g.game_over = true;
-        g.ganhou = false;
-        display_bombs(g.grid);
+        if (!game_over) {
+            game_over = true;
+            ganhou = false;
+            display_bombs();
+        }
+        break;
     }
 }
 
-int main()
+Game::Game(int n_row, int n_col, int num_bombs, const char* title, bool vsync)
+    : row(n_row)
+    , col(n_col)
+    , num_bombs(num_bombs)
+    , ganhou(false)
+    , game_over(false)
+    , window(sf::VideoMode(n_row * CELL_SIZE_BORDER, n_col * CELL_SIZE_BORDER), title, sf::Style::Close)
+    , vertices(4 * n_row * n_col)
 {
-    Game game(window_w, window_h, "Mineclone");
-    game.window.setVerticalSyncEnabled(true);
-    game.texture.loadFromFile("Icons20.png");
-    game.texture.setSmooth(true);
-    game.sprite.setTexture(game.texture);
+    window.setVerticalSyncEnabled(vsync);
+    int8_t* a = new int8_t[row * col];
+    grid = new int8_t*[row];
 
+    grid[0] = &a[0];
+
+    for (int i = 1; i < row; i++) {
+        grid[i] = grid[0] + col * i;
+    }
+}
+
+Game::~Game()
+{
+    delete[] grid[0];
+    delete[] grid;
+}
+
+float get_average_frametime(const std::array<int64_t, 32>& a)
+{
+    return std::accumulate(a.begin(), a.end(), 0) / a.size();
+}
+
+int main(int argc, const char* argv[])
+{
     using namespace std::chrono;
 
+    int col = 16;
+    int row = 16;
+    int num_bombs;
+
+    switch (argc) {
+    case 4:
+        col = atoi(argv[2]);
+        row = atoi(argv[1]);
+        num_bombs = atoi(argv[3]);
+        break;
+
+    case 3:
+        col = atoi(argv[2]);
+        row = atoi(argv[1]);
+        num_bombs = col * row * 0.1;
+        break;
+    default:
+        col = 16;
+        row = 16;
+        num_bombs = col * row * 0.1;
+        break;
+    }
+
+    if (num_bombs > col * row) {
+        num_bombs = col * row * 0.1;
+    }
+
+    Game game(row, col, num_bombs, "Mines", true);
+    game.texture.loadFromFile("Icons.png");
+    game.texture.setSmooth(true);
+    game.set_positions();
+    game.generate_grid();
+
     steady_clock::time_point t1 = steady_clock::now();
-    steady_clock::time_point t2;
 
     int64_t acum = 0;
 
+    constexpr int64_t second = 1'000'000'000;
+    constexpr float dsecond = 1'000'000'000.f;
+
+    char fpsTitle[32] = {};
+    std::array<int64_t, 32> frametimes = {};
+
+    int frametimes_i = 0;
+
     while (game.window.isOpen()) {
-
         steady_clock::time_point t2 = steady_clock::now();
-
         int64_t diff = (t2 - t1).count();
-
         acum += diff;
 
-        if (acum > 1'000'000'000) {
-            // 1s
-            game.window.setTitle(std::to_string(1000000000.0 / diff));
-            acum -= 1'000'000'000;
+        frametimes[frametimes_i] = diff;
+        frametimes_i++;
+        frametimes_i = frametimes_i % 32;
+
+        if (acum > second) {
+            float fps = dsecond / get_average_frametime(frametimes);
+            snprintf(fpsTitle, 32, "Mineclone %f", fps);
+            game.window.setTitle(fpsTitle);
+            acum -= second;
         }
 
         t1 = t2;
@@ -332,11 +393,11 @@ int main()
                 break;
 
             case sf::Event::MouseButtonReleased:
-                handle_mouse_release(ev, game);
+                game.handle_mouse_release(ev);
                 break;
 
             case sf::Event::KeyPressed:
-                handle_keyboard(ev, game);
+                game.handle_keyboard(ev);
                 break;
             }
         }
--- main.h
+++ main.h
@@ -3,47 +3,49 @@
 #include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
 
-constexpr int GRID_COL = 20;
-constexpr int GRID_ROW = 20;
-constexpr int NUM_BOMBS = (GRID_COL * GRID_ROW) * 0.10;
-constexpr int CELL_SIZE_BORDER = 20;
-constexpr int CELL_SIZE = 19;
-constexpr float CELL_SIZEF_BORDER = static_cast<float>(CELL_SIZE_BORDER);
-constexpr float CELL_SIZEF = static_cast<float>(CELL_SIZE);
-constexpr int window_h = GRID_ROW * CELL_SIZE_BORDER;
-constexpr int window_w = GRID_COL * CELL_SIZE_BORDER;
+constexpr static int CELL_SIZE_BORDER = 20;
+constexpr static int CELL_SIZE = 19;
+constexpr static float CELL_SIZEF_BORDER = static_cast<float>(CELL_SIZE_BORDER);
+constexpr static float CELL_SIZEF = static_cast<float>(CELL_SIZE);
 
-using Grid = std::array<std::array<int8_t, GRID_COL>, GRID_ROW>;
+enum CELL_FLAGS {
+    NEAR_BOMB = 0x0F,
+    BOMB = 0x10,
+    FLAG = 0x20,
+    HIDDEN = 0x40,
+};
 
 struct Game {
-    Grid grid;
+    Game(int n_row, int n_col, int num_bombs, const char* title, bool vsync);
+    ~Game();
+
+    int row;
+    int col;
+    int num_bombs;
+    bool ganhou;
+    bool game_over;
     sf::RenderWindow window;
     sf::Texture texture;
-    sf::Sprite sprite;
-    bool game_over;
-    bool ganhou;
+    int8_t** grid;
+    std::vector<sf::Vertex> vertices;
 
-    Game(int w, int h, const char* title);
+    void get_texture(sf::Vertex* v, int8_t cell) const;
+
+    void render();
+
+    bool verifica_ganhou() const;
 
     void draw_grid();
 
-    void get_texture(sf::Vertex* v, int8_t cell);
+    void set_positions();
 
-    void render();
+    void generate_grid();
+
+    void open_cells(int startRow, int startCol);
+
+    void display_bombs();
+
+    void handle_mouse_release(const sf::Event& ev);
+
+    void handle_keyboard(const sf::Event& ev);
 };
-
-bool verifica_ganhou(const Grid& grid);
-
-void draw_grid(Game& game);
-
-void generate_grid(Grid& grid);
-
-void print_values(const Grid& grid);
-
-void open_cells(Grid& grid, int startRow, int startCol);
-
-void display_bombs(Grid& grid);
-
-void handle_mouse_release(const sf::Event& ev, Grid& grid);
-
-void handle_keyboard(const sf::Event& ev, sf::Window& window, Grid& grid);
